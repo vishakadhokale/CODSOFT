fc49f31b97e981a0de38657450dbf99e8469cd4f
vishakadhokale <dhokalevishaka@gmail.com>
2024-08-04 16:48:41 +0530

    Add files via upload

---
+++ CineEpic/script.js
@@ -0,0 +1,127 @@
+let movies = [
+  {
+    name: "lapaata ladies",
+    des: "Two newlyweds find themselves inadvertently separated from their husbands.",
+    image: "./images/LL.jpg",
+  },
+  {
+    name: "Tumse Na Ho Payega",
+    des: "Gaurav and his friends rediscover themselves when they decide to defy the expectations of society.",
+    image: "./images/TNHP.JPG",
+  },
+  {
+    name: "Shiddat",
+    des: "Love-struck Jaggi can cross the seven seas for his dream girl, Kartika.",
+    image: "./images/s.jpg",
+  },
+
+  {
+    name: "The Great Indian Murder",
+    des: "Raya and the Last Dragon is an animated Disney film released in 2021.",
+    image: "./images/tgim.jpg",
+  },
+
+  {
+    name: "Cuttputlli",
+    des: "The quiet,beautiful,exotic and lush green valley of Kasauli is struck by gruesome and mysterious murders.",
+    image: "./images/c.jpg",
+  },
+];
+
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0; // track the current slide
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  // create DOM Elements
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  // attaching all element
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  // setting up images
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  // setting elements classnames
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+// video cards
+
+const videoCards = document.querySelectorAll(".video-card");
+
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+// cards sliders
+
+let cardContainers = document.querySelectorAll(".card-container");
+let preBtns = document.querySelectorAll(".pre-btn");
+let nxtBtns = document.querySelectorAll(".nxt-btn");
+
+// Function to scroll the card container to the left
+function scrollLeft(container) {
+  container.scrollLeft -= container.clientWidth;
+}
+
+// Function to scroll the card container to the right
+function scrollRight(container) {
+  container.scrollLeft += container.clientWidth;
+}
+
+// Attach click event listeners to previous and next buttons
+preBtns.forEach((preBtn, index) => {
+  preBtn.addEventListener("click", () => {
+    scrollLeft(cardContainers[index]);
+  });
+});
+
+nxtBtns.forEach((nxtBtn, index) => {
+  nxtBtn.addEventListener("click", () => {
+    scrollRight(cardContainers[index]);
+  });
+});
